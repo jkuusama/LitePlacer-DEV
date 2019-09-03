5c5ce74645934f866aba221ae23ef96b90e83d3e
Juha Kuusama <juha@liteplacer.com>
2019-09-03 15:14:58 +0300

    Better diagnostics on failed homing

--- LitePlacer/MainForm.cs
+++ LitePlacer/MainForm.cs
@@ -2973,11 +2973,19 @@ namespace LitePlacer
                     DisplayText("X: " + X.ToString("0.000", CultureInfo.InvariantCulture)
                         + ", Y: " + Y.ToString("0.000", CultureInfo.InvariantCulture));
                 }
+                else if (res == 0)
+                {
+                    DisplayText("Measurement " + Tries.ToString(CultureInfo.InvariantCulture) + ", no result");
+                }
+                else
+                {
+                    DisplayText("Measurement " + Tries.ToString(CultureInfo.InvariantCulture) + ", multiple results (" + res.ToString(CultureInfo.InvariantCulture) + ")");
+                }
             }
             while ((Successes<7)&&(Tries<20));
             if (Tries >= 20)
             {
-                DisplayText("Optical homing failed, 20 tries did not give 7 results.");
+                DisplayText("Optical homing failed, 20 tries did not give 7 unique results.");
                 return false;
             }
             Xlist.Sort();
