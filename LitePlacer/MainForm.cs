9e00381dbbdd8e7c16ce283dff741205cf3b0354
Juha Kuusama <juha@liteplacer.com>
2019-09-03 15:05:19 +0300

    Zguard works again

--- LitePlacer/MainForm.cs
+++ LitePlacer/MainForm.cs
@@ -1419,7 +1419,10 @@ namespace LitePlacer
                 }
             }
 
-            if ((Cnc.CurrentZ > 5) && !((e.KeyCode == Keys.F11) || (e.KeyCode == Keys.F12)))  // F11&F12: It is ok to move z manually
+            if (
+                ((Cnc.CurrentZ > 5) && ZguardIsOn())
+                &&
+                !((e.KeyCode == Keys.F11) || (e.KeyCode == Keys.F12)))  // F11&F12: It is ok to move z manually
             {
                 DisplayText("Nozzle is down", KnownColor.DarkRed, true);
                 return;
@@ -2492,6 +2495,10 @@ namespace LitePlacer
         }
 
         private bool _Zguard = true;
+        private bool ZguardIsOn()
+        {
+            return _Zguard;
+        }
         private void ZGuardOn()
         {
             _Zguard = true;
@@ -2503,7 +2510,7 @@ namespace LitePlacer
 
         private bool CNC_NozzleIsDown_m()
         {
-            if ((Cnc.CurrentZ > 5) && _Zguard)
+            if ((Cnc.CurrentZ > 5) && ZguardIsOn())
             {
                 DisplayText("Nozzle down error.");
                 ShowMessageBox(
