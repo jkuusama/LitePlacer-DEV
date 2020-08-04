1609dbc1a02876bd969b17af9ac301ff85508867
KingMetal7 <philipschulze@web.de>
2020-08-04 12:48:22 +0200

    -Fiducial measurement manually adjustable

---
+++ LitePlacer/FiducialAutoForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace LitePlacer
+{
+    partial class FiducialAutoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConfirm = new System.Windows.Forms.Button();
+            this.buttonRetry = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonManual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonConfirm
+            // 
+            this.buttonConfirm.Location = new System.Drawing.Point(12, 87);
+            this.buttonConfirm.Name = "buttonConfirm";
+            this.buttonConfirm.Size = new System.Drawing.Size(75, 23);
+            this.buttonConfirm.TabIndex = 0;
+            this.buttonConfirm.Text = "Confirm";
+            this.buttonConfirm.UseVisualStyleBackColor = true;
+            this.buttonConfirm.Click += new System.EventHandler(this.buttonConfirm_Click);
+            // 
+            // buttonRetry
+            // 
+            this.buttonRetry.Location = new System.Drawing.Point(174, 87);
+            this.buttonRetry.Name = "buttonRetry";
+            this.buttonRetry.Size = new System.Drawing.Size(75, 23);
+            this.buttonRetry.TabIndex = 1;
+            this.buttonRetry.Text = "Retry";
+            this.buttonRetry.UseVisualStyleBackColor = true;
+            this.buttonRetry.Click += new System.EventHandler(this.buttonRetry_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(255, 87);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.MinimumSize = new System.Drawing.Size(293, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 52);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fiducial location OK?\r\n\r\nAlternatively to specify a manual location move the masc" +
+    "hine\r\nto the desired position and confirm with \"Manual\"";
+            // 
+            // buttonManual
+            // 
+            this.buttonManual.Location = new System.Drawing.Point(93, 87);
+            this.buttonManual.Name = "buttonManual";
+            this.buttonManual.Size = new System.Drawing.Size(75, 23);
+            this.buttonManual.TabIndex = 1;
+            this.buttonManual.Text = "Manual";
+            this.buttonManual.UseVisualStyleBackColor = true;
+            this.buttonManual.Click += new System.EventHandler(this.buttonManual_Click);
+            // 
+            // FiducialAutoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 122);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonManual);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonRetry);
+            this.Controls.Add(this.buttonConfirm);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(358, 161);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(358, 161);
+            this.Name = "FiducialAutoForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Confirm Fiducial";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FiducialAutoForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConfirm;
+        private System.Windows.Forms.Button buttonRetry;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonManual;
+    }
+}
---
+++ LitePlacer/FiducialAutoForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LitePlacer
+{
+    public partial class FiducialAutoForm : Form
+    {
+        public enum FiducialAutoResult
+        {
+            None, OK, Retry, Cancel, Manual
+        }
+
+        public bool dialogFinished = false;
+        private FiducialAutoResult dialogResult = FiducialAutoResult.None;
+
+        new public FiducialAutoResult DialogResult { get => dialogResult; set => dialogResult = value; }
+
+        public FiducialAutoForm()
+        {
+            InitializeComponent();
+        }
+
+        public FiducialAutoForm(bool validMeasuredLocation)
+        {
+            InitializeComponent();
+            if (!validMeasuredLocation)
+            {
+                buttonConfirm.Enabled = false;
+                label1.Text = "Can't regognize fiducial. Retry or manually move the maschine\r\n" +
+                              "to the fiducial and confirm with \"Manual\".";
+            }
+            else
+            {
+                label1.Text = "Fiducial location OK?\r\n\r\nAlternatively to specify a manual location move the masc" +
+                            "hine\r\nto the desired position and confirm with \"Manual\".";
+            }
+        }
+
+        private void buttonConfirm_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = FiducialAutoResult.OK;
+            this.Close();
+        }
+
+        private void buttonRetry_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = FiducialAutoResult.Retry;
+            this.Close();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = FiducialAutoResult.Cancel;
+            this.Close();
+        }
+
+        private void buttonManual_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = FiducialAutoResult.Manual;
+            this.Close();
+        }
+
+        private void FiducialAutoForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            dialogFinished = true;
+        }
+    }
+}
